90f869ad34f95049099a1bc7a3477574b88be2ea
Djalves424 <djalves424@gmail.com>
2024-06-24 14:04:27 -0300

    Updated project

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./routes/Home";
+import HomeBody from "./routes/Home/HomeBody";
+import Promotion from "./routes/Home/Promotion";
+
 function App() {
   
-  return (
-<h1>Bem vindo</h1>
-  )
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} >
+            <Route index element={<HomeBody />} />
+            <Route path="promotion" element={<Promotion />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    );
 }
 
 export default App
